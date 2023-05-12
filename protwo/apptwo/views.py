12f3242ea5a4cce35b6bb4c082381c7d755838d9
Babayaga <medasani.r26@gmail.com>
2023-05-12 22:38:18 +0530

    changes

--- protwo/apptwo/views.py
+++ protwo/apptwo/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<em>Hey boi</em>")
 
+
+def help(request):
+    helpdict = {'help_insert' :'HELP PAGE'}
+    return render(request,'apptwo/help.html',context = helpdict)
 # Create your views here.
--- protwo/protwo/urls.py
+++ protwo/protwo/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 from apptwo import views
 
 
 urlpatterns = [
+    path('help/',include('apptwo.urls')),
     path('',views.index,name='index'),
     path('admin/', admin.site.urls),
 ]
